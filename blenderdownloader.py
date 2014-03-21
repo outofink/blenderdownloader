f6db0328c160c17631261a80909e8ec7ffd9e18e
Out of Ink Software <admin@outofinksoftware.com>
2014-03-20 21:37:11 -0400

    initial commit

---
+++ blenderdownloader.py
@@ -0,0 +1,105 @@
+from bs4 import BeautifulSoup as BS
+import urllib2, sys, getopt
+from os import system, name, path, remove
+import zipfile
+def main(argv):
+	opts, args = getopt.getopt(argv, "hv:", ["help", "version="])
+	grammar = ""
+	for opt, arg in opts:                
+		if opt in ("-h", "--help"):
+			print "You thought there was help!? Well, there will be, but not yet."                          
+			sys.exit()                                    
+		elif opt in ("-v", "--version"):
+			grammar = arg
+
+	system('cls' if name == 'nt' else 'clear')
+	print "Blender Downloader v1.0"
+	print "Retrieving most recent Blender version..."
+	try:
+		blender_html = BS(urllib2.urlopen(urllib2.Request('http://www.blender.org')).read())
+	except urllib2.URLError:
+		print "\nERROR: Not connected to the internet."
+		sys.exit(1)
+	blenderCurVer = blender_html.find('div', attrs={'class':"btn btn-large"}).text.strip()
+	print "\nMost recent version is %s." % blenderCurVer
+	blenderCurVerNum = float(blenderCurVer.replace("Blender ",""))
+
+
+	def genlink(version=blenderCurVer.replace("Blender ",""), platform="windows64"):
+		gversion=version.replace("a","").replace("b","")
+
+		final="http://download.blender.org/release/Blender{0}/blender-{2}-{1}.zip".format(gversion, platform, version)
+		try:
+			u = urllib2.urlopen(final)
+		except urllib2.HTTPError:
+			try:
+				final="http://download.blender.org/release/Blender{0}/blender-{2}-release-{1}.zip".format(gversion, platform, version)
+				u = urllib2.urlopen(final)
+			except urllib2.HTTPError:
+				print "ERROR: Blender version does not exist.\nNote: Blender Downloader only supports Blender 2.57+"
+				return False
+			return final
+		return final	
+	def download(link="http://download.blender.org/release/Blender2.70/blender-2.70-windows64.zip"):
+		file_name = link.split('/')[-1]
+		u = urllib2.urlopen(link)
+		f = open(file_name, 'wb')
+		meta = u.info()
+		file_size = int(meta.getheaders("Content-Length")[0])
+		file_sizeMB=file_size/1024.0/1024.0
+		print("Downloading: {0} filesize: {1:.4}MB".format(file_name, file_sizeMB))
+
+		file_size_dl = 0
+		block_sz = 8192
+		while True:
+		    buffer = u.read(block_sz)
+		    if not buffer:
+		        break
+
+		    file_size_dl += len(buffer)
+		    f.write(buffer)
+		    p = float(file_size_dl) / file_size
+		    status = r"{0:.2f}MB/{2:.2f}MB [{1:.2%}]       ".format(file_size_dl/1024.0/1024.0, p, file_sizeMB)
+		    status = status + chr(8)*(len(status)+1)
+		    sys.stdout.write(status)
+		print
+		f.close()
+		return file_name
+
+	def extract(zipped="blender-2.70-windows64.zip"):
+		zip = zipfile.ZipFile(zipped)
+		nameList = zip.namelist()
+		fileCount = len(nameList)
+		print "\nExtracting: %s filecount: %s" % (zipped, fileCount)
+		count = 0
+
+		for item in nameList:
+			count += 1
+			dir,file = path.split(item)
+
+			lolz =r"{0}/{1} files unzipped [{2:.2%}]".format(count, fileCount, (count*1.0)/fileCount)
+			lolz = lolz + chr(8)*(len(lolz)+1)
+			sys.stdout.write(lolz) 
+			zip.extract(item,"")
+		print
+		return True
+	#stuff
+	if grammar:
+		linked=genlink(grammar)
+	else:
+		linked=genlink()
+	if linked:
+		if grammar:
+			print "Downloading Blender %s now...\n" % grammar
+		else:
+			print "Downloading most recent version now... (please wait a bit)\n"
+		blenderzip=download(linked)
+		print "\nExtracting blender package..."
+		#other stuff
+		extract(blenderzip)
+		print "\nCleaning up..."
+		remove(blenderzip)
+		print "\nDone!"
+	
+if __name__ == "__main__":
+	main(sys.argv[1:])
